f677082c02fa857c05141d12ecd30c2d057c89ac
tusharpingale <tushar@insider.in>
2021-11-18 10:25:33 +0530

    added build src and dependencies

---
+++ buildSrc/src/main/java/Android.kt
@@ -0,0 +1,8 @@
+object Android {
+    const val appId = "com.tushar.modularizationcompose"
+    const val compileSdk = 31
+    const val minSdk = 21
+    const val targetSdk = 31
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
